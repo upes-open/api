7ce7f8683be5142abdc386b646c6fdc42b2784f5
Cheiras <dtcheche@gmail.com>
2020-10-04 11:57:57 -0300

    Modify view to use ModelViewSet

--- api/views.py
+++ api/views.py
@@ -1,27 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.generics import (
-  ListAPIView, 
-  RetrieveAPIView,
-  UpdateAPIView,
-  RetrieveUpdateAPIView,
-  ListCreateAPIView
-)
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from api import models, serializers
-
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
+from api.models import Mem
+from api.serializers import MemberSerializer
+
+
 def index(request):
     return HttpResponse('Hello')
 
-class MemberDetailView(RetrieveUpdateAPIView):
-  queryset = models.Mem.objects.all()
-  serializer_class = serializers.MemberSerializer 
 
-@api_view()
-def userApi(request):
-    users = models.Mem.objects.all()
-    response = serializers.MemberSerializer(users, many = True)
-    return Response(response.data)
+class MemberDetailView(ModelViewSet):
+    queryset = Mem.objects.all()
+    serializer_class = MemberSerializer
